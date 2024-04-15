d54ebb472de9591ca77aca0a655a809aedb3880d
angelinelabellegosse <angelineali91@gmail.com>
2024-04-15 11:51:37 +0200

    le fichier header de notre fichier c++ princ

--- src/parse_yaml.h
+++ src/parse_yaml.h
@@ -1,20 +1,48 @@
-class TypeName {
+#include <string>
+#include <vector>
+
+class Function {
+public:
+    std::string file;
+    std::string name;
+    std::vector<std::string> protections;
 };
 
-class LectureYAML {
+class Parameter {
+public:
+    std::string file;
+    std::string name;
+    std::string functionName;
+    std::vector<std::string> protections;
 };
 
-class Creation {
+class Global {
+public:
+    std::string file;
+    std::string name;
+    std::vector<std::string> protections;
 };
 
-class Separation {
+class Structure {
+public:
+    std::string file;
+    std::string name;
+    std::vector<std::string> protections;
 };
 
-class Exception {
+class BlackList {
+public:
+    std::vector<std::string> files;
+    std::vector<std::string> protections;
 };
 
-class EcritureYAML {
-};
 
-class Check {
-};
+
+
+/*
+Function : représente une fonction avec les protections associées.
+Parameter : représente un paramètre de fonction avec les protections associées.
+Global : représente une variable globale avec les protections associées.
+Structure : représente une structure avec les protections associées.
+BlackList : représente une liste noire de fichiers avec les protections associées.
+*/
