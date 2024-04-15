f40e140819097406a6150dd32e0dc405e5435e04
meyan03 <meyanmariam04@gmail.com>
2024-04-15 12:03:59 +0200

    le fichier parse_yaml.h mis à jour

--- src/parse_yaml.h
+++ src/parse_yaml.h
@@ -1,39 +1,56 @@
 #include <string>
 #include <vector>
 
-class Function {
+// Déclarations des classes
+class Option {
 public:
-    std::string file;
     std::string name;
-    std::vector<std::string> protections;
+    std::string value;
+    Option(std::string name, std::string value) : name(name), value(value) {}
 };
 
-class Parameter {
+class BlackFile {
 public:
-    std::string file;
     std::string name;
+    std::string protection;
+    int value;
+    BlackFile(std::string name, std::string protection, int value) : name(name), protection(protection), value(value) {}
+};
+
+class Function {
+public:
+    std::string fileName;
     std::string functionName;
-    std::vector<std::string> protections;
+    std::vector<std::string> parameters;
+    Function(std::string fileName, std::string functionName, std::vector<std::string> parameters) : fileName(fileName), functionName(functionName), parameters(parameters) {}
+};
+
+class TypeDef {
+public:
+    std::string fileName;
+    std::string typedefName;
+    std::vector<std::string> protection;
+    TypeDef(std::string fileName, std::string typedefName, std::vector<std::string> protection) : fileName(fileName), typedefName(typedefName), protection(protection) {}
+    
+    std::string getTypeDefName() const {
+        return typedefName;
+    }
 };
 
 class Global {
 public:
-    std::string file;
-    std::string name;
-    std::vector<std::string> protections;
+    std::string fileName;
+    std::string globalName;
+    std::vector<std::string> protection;
+    Global(std::string fileName, std::string globalName, std::vector<std::string> protection) : fileName(fileName), globalName(globalName), protection(protection) {}
 };
 
 class Structure {
 public:
-    std::string file;
-    std::string name;
-    std::vector<std::string> protections;
-};
-
-class BlackList {
-public:
-    std::vector<std::string> files;
-    std::vector<std::string> protections;
+    std::string fileName;
+    std::string structureName;
+    std::vector<std::string> protection;
+    Structure(std::string fileName, std::string structureName, std::vector<std::string> protection) : fileName(fileName), structureName(structureName), protection(protection) {}
 };
 
 
